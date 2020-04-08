8078a441d600c6324fb7e4d887e5f3c2dbd78a12
Darth-Revan-13 <alexander.hilbert@bryanuniversity.edu>
2020-04-08 11:02:19 -0400

    Pause and practice assignment

---
+++ week-1/pause-and-practice/index.js
@@ -0,0 +1,9 @@
+var shopper = {
+    name: "Karen",
+    age: 50,
+    wantsToSpeakToManager: true,
+    items: function() {
+        return groceryCart
+    }
+}
+var groceryCart = ["Too much toilet paper", "Too much hand sanitizer", "Many face masks", "Many gloves"]
