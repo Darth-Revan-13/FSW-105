b058c2cca7a2dfda7118a84fa902be7fcee8b5ad
Darth-Revan-13 <alexander.hilbert@bryanuniversity.edu>
2020-04-16 10:46:49 -0400

    Debugging code complete

---
+++ week-2/Pause-and-Practice-If-Statement-Olympics/index.js
@@ -0,0 +1,24 @@
+if(5 > 3) {
+    console.log("Is greater than")
+}
+
+if('cat'.length = 3) {
+console.log("Is the length")
+}
+
+if('cat' = 'dog') {
+    console.log("Is equal")
+} else {
+    console.log("not the same")
+}
+
+var person = {
+    name: "Bobby" ,
+    age: 12
+}
+
+if(person >= 18) {
+    console.log("Bobby is allowed to go into movies")
+} else {
+    console.log("Bobby is not allowed to go into movies")
+}
